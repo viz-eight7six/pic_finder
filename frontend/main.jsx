0ee23dfe07ef29b7047874ce708b3a09cbdaa68b
Vinson Chen <viz@Vinsons-MacBook-Pro.local>
2017-06-05 13:55:40 -0700

    remove hidden console log

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -40,7 +40,7 @@ class MainPage extends React.Component {
              }})
      .done((response) => {
         let data = response.images;
-        this.setState({images: data}, console.log(data));
+        this.setState({images: data});
      })
      .fail(function(data){
          alert(JSON.stringify(data,2));
